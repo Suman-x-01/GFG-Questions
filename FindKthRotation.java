a316a25972fe1b8e2147b3308395738b63d62f70
Suman <sumannayak192113114@gmail.com>
2025-02-02 01:26:06 +0530

    Add First

--- FindKthRotation.java
+++ FindKthRotation.java
@@ -2,6 +2,7 @@ package com.GFG;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /*
 Link : https://www.geeksforgeeks.org/problems/rotation4723/1
  */
@@ -17,16 +18,18 @@ public class FindKthRotation {
 		arr.add(4);
 		int rotation = binarySearch(arr);
 		System.out.println(rotation);
-//		System.out.println(arr.size());
+		// System.out.println(arr.size());
 	}
 
 	private static int findRotation(List<Integer> arr) {
-		if (arr.isEmpty()) return -1;
+		if (arr.isEmpty())
+			return -1;
 		int start = 0, end = arr.size() - 1;
 		while (start <= end) {
-			int mid=start+(end-start)/2;
-			if (arr.get(0) < arr.get(arr.size() - 1)) return 0;
-//			[5,6,7,8, 1, 2, 3, 4]
+			int mid = start + (end - start) / 2;
+			if (arr.get(0) < arr.get(arr.size() - 1))
+				return 0;
+			// [5,6,7,8, 1, 2, 3, 4]
 			else {
 				int small = binarySearch(arr);
 
@@ -34,21 +37,22 @@ public class FindKthRotation {
 		}
 		return 0;
 	}
-//	Used to find the smallest element
-	public static int binarySearch(List<Integer>arr){
-		int start=0, end=arr.size()-1;
-		if (!arr.isEmpty()){
-			while (start<end){
-				int mid=start+(end-start)/2;
 
-				if(arr.get(mid)>arr.get(end)){
-					start=mid+1;
-				}else {
-					end=mid;
+	// Used to find the smallest element
+	public static int binarySearch(List<Integer> arr) {
+		int start = 0, end = arr.size() - 1;
+		if (!arr.isEmpty()) {
+			while (start < end) {
+				int mid = start + (end - start) / 2;
+
+				if (arr.get(mid) > arr.get(end)) {
+					start = mid + 1;
+				} else {
+					end = mid;
 				}
 			}
 			return end;
-		}else{
+		} else {
 			return 0;
 		}
 	}

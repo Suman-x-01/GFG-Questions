7c93c5c2a99cb139af8ddce96e1bafa4c9457beb
Suman <sumannayak192113114@gmail.com>
2025-02-03 23:13:46 +0530

    Add 2nd

---
+++ Aggressive_cows.java
@@ -0,0 +1,56 @@
+package com.demo2;
+
+import java.util.Arrays;
+
+/*
+Link :https://www.geeksforgeeks.org/problems/aggressive-cows/1
+ */
+public class Aggressive_cows {
+    public static void main(String[] args) {
+        int[] arr = { 10, 1, 2, 7, 5 };
+        int k = 3;
+        int i = aggressiveCows(arr, k);
+        System.out.println(i);
+    }
+
+    public static int aggressiveCows(int[] stalls, int k) {
+        // code here
+        // if stalls is less than cow then return false
+        if (k > stalls.length) {
+            return -1;
+        }
+        Arrays.sort(stalls);
+        int start = 1, end = stalls[stalls.length - 1] - stalls[0];
+        int ans = 0;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (findCowMinDistance(stalls, mid, k)) {
+                ans = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private static boolean findCowMinDistance(int[] stalls, int mid, int noOfCows) {
+        // by default, we put a cow at the first index
+        int lastCowStand = stalls[0];
+        int cowCount = 1;
+        for (int i = 1; i < stalls.length; i++) {
+            // check if from the last index where the cow is stand and the current index is
+            // greater than the mid
+            if (stalls[i] - lastCowStand >= mid) {
+                cowCount += 1;
+                lastCowStand = stalls[i];
+            }
+        }
+        // if all the cows are put, then return true
+        if (cowCount >= noOfCows)
+            return true;
+        else
+            return false;
+
+    }
+}
